fe94835d48251132b4a521e561df639d3e79499c
Tokuda Hikaru <hkrtkd@gmail.com>
2022-02-08 00:24:35 +0900

    FireBase関係のパッケージアップデート対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/top_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyCTtWUHKBF1ttD3jRfffiNaNdXPSIHyyPg",
+        appId: "1:781222392442:web:a6fcf3b9820dcb83f4c1c3",
+        messagingSenderId: "781222392442",
+        projectId: "fir-task-app-5cbc4"
+    )
+  );
   runApp(MyApp());
 }
 
