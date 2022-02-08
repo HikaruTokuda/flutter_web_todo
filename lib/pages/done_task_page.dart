8e90189d6e7ddd92a1dc96a0031a35d26182513c
Tokuda Hikaru <hkrtkd@gmail.com>
2022-02-08 17:39:12 +0900

    完了タスクリストでもリアルタイム更新を実装

--- lib/pages/done_task_page.dart
+++ lib/pages/done_task_page.dart
@@ -11,135 +11,121 @@ class DoneTaskPage extends StatefulWidget {
 
 class _DoneTaskPageState extends State<DoneTaskPage> {
   TextEditingController editTitleController = TextEditingController();
-  List<Task> undoneTaskList = [];
-  List<Task> doneTaskList = [];
-
-  Future<void> getDoneTasks() async {
-    var collection = FirebaseFirestore.instance.collection('task');
-    var snapshot = await collection.where('is_done', isEqualTo: true).get();
-    snapshot.docs.forEach((task) {
-      Task doneTask = Task(
-        title: task['title'],
-        isDone: task['is_done'],
-        createdTime: task['created_time'],
-      );
-      doneTaskList.add(doneTask);
-    });
-    setState(() {});
-  }
+  CollectionReference? tasks;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getDoneTasks();
+    tasks = FirebaseFirestore.instance.collection('task');
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: doneTaskList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return CheckboxListTile(
-            controlAffinity: ListTileControlAffinity.leading,
-            title: Text(doneTaskList[index].title!),
-            value: doneTaskList[index].isDone,
-            onChanged: (bool? value) {
-              doneTaskList[index].isDone = value;
-              undoneTaskList.add(doneTaskList[index]);
-              doneTaskList.removeAt(index);
-              setState(() {});
-            },
-            secondary: IconButton(
-              icon: Icon(Icons.more_horiz),
-              onPressed: () {
-                showModalBottomSheet(context: context, builder: (context) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        title: Text('編集'),
-                        leading: Icon(Icons.edit),
-                        onTap: () {
-                          // ボトムシートを非表示
-                          Navigator.pop(context);
-                          // 編集用ダイアログの表示
-                          showDialog(context: context, builder: (context) {
-                            return SimpleDialog(
-                              titlePadding: EdgeInsets.all(20),
-                              title: Container(
-                                color: Colors.white,
-                                child: Column(
-                                  children: [
-                                    Text('タイトルを編集'),
-                                    Container(
-                                        width: 500,
-                                        child: TextField(
-                                          controller: editTitleController,
-                                          decoration: InputDecoration(
-                                              border: OutlineInputBorder()
+    return StreamBuilder<QuerySnapshot>(
+      stream: tasks?.snapshots(),
+      builder: (context, snapshot) {
+        if(snapshot.hasData) {
+          return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                if(!snapshot.data?.docs[index]['is_done']) return Container();
+                return CheckboxListTile(
+                  controlAffinity: ListTileControlAffinity.leading,
+                  title: Text(snapshot.data?.docs[index]['title']),
+                  value: snapshot.data?.docs[index]['is_done'],
+                  onChanged: (bool? value) {
+                  },
+                  secondary: IconButton(
+                    icon: Icon(Icons.more_horiz),
+                    onPressed: () {
+                      showModalBottomSheet(context: context, builder: (context) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ListTile(
+                              title: Text('編集'),
+                              leading: Icon(Icons.edit),
+                              onTap: () {
+                                // ボトムシートを非表示
+                                Navigator.pop(context);
+                                // 編集用ダイアログの表示
+                                showDialog(context: context, builder: (context) {
+                                  return SimpleDialog(
+                                    titlePadding: EdgeInsets.all(20),
+                                    title: Container(
+                                      color: Colors.white,
+                                      child: Column(
+                                        children: [
+                                          Text('タイトルを編集'),
+                                          Container(
+                                              width: 500,
+                                              child: TextField(
+                                                controller: editTitleController,
+                                                decoration: InputDecoration(
+                                                    border: OutlineInputBorder()
+                                                ),
+                                              )
                                           ),
-                                        )
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 30.0),
-                                      child: Container(
-                                          width: 200,
-                                          height: 30,
-                                          child: ElevatedButton(
-                                              onPressed: (){
-                                                doneTaskList[index].title = editTitleController.text;
-                                                Navigator.pop(context);
-                                                setState(() {});
-                                              },
-                                              child: Text('編集')
+                                          Padding(
+                                            padding: const EdgeInsets.only(top: 30.0),
+                                            child: Container(
+                                                width: 200,
+                                                height: 30,
+                                                child: ElevatedButton(
+                                                    onPressed: (){
+                                                    },
+                                                    child: Text('編集')
+                                                )
+                                            ),
                                           )
+                                        ],
                                       ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            );
-                          });
-                        },
-                      ),
-                      ListTile(
-                        title: Text('削除'),
-                        leading: Icon(Icons.delete),
-                        onTap: () {
-                          // ボトムシートを非表示に
-                          Navigator.pop(context);
-                          // 確認用ダイアログの表示
-                          showDialog(context: context, builder: (context) {
-                            return AlertDialog(
-                              title: Text('${doneTaskList[index].title}を削除しますか？'),
-                              actions: [    // アラートダイアログに設置するボタン
-                                TextButton(
-                                    onPressed: (){
-                                      doneTaskList.removeAt(index);
-                                      Navigator.pop(context);
-                                      setState(() {});
-                                    },
-                                    child: Text('はい')
-                                ),
-                                TextButton(
-                                    onPressed: (){
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text('キャンセル')
-                                )
-                              ],
-                            );
-                          });
-                        },
-                      )
-                    ],
-                  );
-                });
-              },
-            ),
+                                    ),
+                                  );
+                                });
+                              },
+                            ),
+                            ListTile(
+                              title: Text('削除'),
+                              leading: Icon(Icons.delete),
+                              onTap: () {
+                                // ボトムシートを非表示に
+                                Navigator.pop(context);
+                                // 確認用ダイアログの表示
+                                showDialog(context: context, builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('${snapshot.data?.docs[index]['title']}を削除しますか？'),
+                                    actions: [    // アラートダイアログに設置するボタン
+                                      TextButton(
+                                          onPressed: (){
+                                          },
+                                          child: Text('はい')
+                                      ),
+                                      TextButton(
+                                          onPressed: (){
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text('キャンセル')
+                                      )
+                                    ],
+                                  );
+                                });
+                              },
+                            )
+                          ],
+                        );
+                      });
+                    },
+                  ),
+                );
+              }
           );
+        } else {
+          return Container();
         }
+
+      }
     );
   }
 }
